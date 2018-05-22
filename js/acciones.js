fcfa1c24764a6feadb0e5442cfc066516ca984f1
Michael <michaelalonsoo18@gmail.com>
2018-05-22 08:00:57 -0500

    2subida
    dos

--- js/acciones.js
+++ js/acciones.js
@@ -1,23 +1,22 @@
 // JavaScript Document
-
-$(document).ready(function(e) {
-	document.addEventListener("deviceready", function(){
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
 		$('#izquierda').on("swipeleft",function(){
-			navigator.notification.alert("Deslizó a la izquierda",function(){"aplicación7","Aceptar"});//cierre deslizar izquierda
-			$('#derecha').on("swiperight",function(){
-			navigator.notification.confirm("¿Qué quieres hacer?",function(opt){
-				switch(opt)
-				{
-					case 1:
-					navigator.notification.beep(2);
-					break;
-					
-					case 2:
-					navigator.notification.vibrate(2000);
-					break;
-				}
-			},"Aplicación7","Beepear,Vibrar,Cancelar");
-			});//cierre del deslizar derecha
-		},false); //cierre del deviceready
-	});
-});
+			navigator.notification.alert("Delizó a la izquierda",function(){"aplicación7","Aceptar"});
+		});	//cierre de la izquierda
+		$('#derecha').on("swiperight",function(){	
+		navigator.notification.confirm("¿Qué quieres hacer?",function(opt){
+			switch(opt)
+			{
+			  case 1:
+			  navigator.notification.beep(2);
+			  break;
+			    
+			  case 2:
+			  navigator.notification.vibrate(1000);
+			  break;
+			}
+		},"Aplication7","Beepear,Vibrar,Cancelar");
+	});//cierre del deslizar derecha
+	},false);//Cierre del deviceready
+});//cierre del document principal
